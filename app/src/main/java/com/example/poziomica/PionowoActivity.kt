51bf05225ceff4efd4fbe3cb57dd69c31f1b0c16
WojciechCinal <wojtekc44@gmail.com>
2023-05-28 19:35:07 +0200

    version 1.0
    - probably final version

--- app/src/main/java/com/example/poziomica/PionowoActivity.kt
+++ app/src/main/java/com/example/poziomica/PionowoActivity.kt
@@ -1,6 +1,8 @@
 package com.example.poziomica
 
 import android.content.Context
+import android.content.Intent
+import android.graphics.PorterDuff
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -11,6 +13,8 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatDelegate
 import android.os.Vibrator
+import android.widget.ImageButton
+import androidx.core.content.ContextCompat
 
 class PionowoActivity: AppCompatActivity(), SensorEventListener {
 
@@ -19,6 +23,8 @@ class PionowoActivity: AppCompatActivity(), SensorEventListener {
     private lateinit var info: TextView
     private lateinit var kolo: ImageView
     private var ostatniPomiar: Long = 0
+    private lateinit var buttonHome: ImageButton
+    private lateinit var buttonZamiana: ImageButton
 
 
 
@@ -32,6 +38,18 @@ class PionowoActivity: AppCompatActivity(), SensorEventListener {
         kwadrat = findViewById(R.id.kwadrat_pomiarowy)
         kolo = findViewById(R.id.kolo_pomiarowe)
         info = findViewById(R.id.info)
+
+        buttonHome = findViewById(R.id.button_pause)
+        buttonZamiana = findViewById(R.id.buton_zamiana)
+
+        buttonHome.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        buttonZamiana.setOnClickListener {
+            val intent = Intent(this, PoziomoActivity::class.java)
+            startActivity(intent)
+        }
 
         setUpSensorStuff()
     }
@@ -75,8 +93,10 @@ class PionowoActivity: AppCompatActivity(), SensorEventListener {
                     val pattern = longArrayOf(50, 150,50)
                     // Wywołaj wibrację
                     vibrator.vibrate(pattern, -1)
+                    info.background.setColorFilter(ContextCompat.getColor(this, R.color.green), PorterDuff.Mode.SRC_ATOP)
                 }else{
                     info.text = "Płaszczyzna nie jest w normie"
+                    info.background.setColorFilter(ContextCompat.getColor(this, R.color.red), PorterDuff.Mode.SRC_ATOP)
                 }
                 if(goraDol == 0.0){
                     info.text="Idealnie w poziomie"
--- app/src/main/java/com/example/poziomica/PoziomoActivity.kt
+++ app/src/main/java/com/example/poziomica/PoziomoActivity.kt
@@ -1,6 +1,9 @@
 package com.example.poziomica
 
 import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -8,10 +11,11 @@ import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Vibrator
-import android.view.Surface
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
 
 class PoziomoActivity : AppCompatActivity(), SensorEventListener {
 
@@ -20,6 +24,8 @@ class PoziomoActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var info: TextView
     private lateinit var kolo: ImageView
     private var ostatniPomiar: Long = 0
+    private lateinit var buttonHome: ImageButton
+    private lateinit var buttonZamiana: ImageButton
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +38,18 @@ class PoziomoActivity : AppCompatActivity(), SensorEventListener {
         kwadrat = findViewById(R.id.kwadrat_pomiarowy)
         kolo = findViewById(R.id.kolo_pomiarowe)
         info = findViewById(R.id.info)
+
+        buttonHome = findViewById(R.id.button_pause)
+        buttonZamiana = findViewById(R.id.buton_zamiana)
+
+        buttonHome.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        buttonZamiana.setOnClickListener {
+            val intent = Intent(this, PionowoActivity::class.java)
+            startActivity(intent)
+        }
 
         setUpSensorStuff()
     }
@@ -59,77 +77,41 @@ class PoziomoActivity : AppCompatActivity(), SensorEventListener {
             if (curTime - ostatniPomiar >= 600) {
                 ostatniPomiar = curTime
 
-                val orientation = windowManager.defaultDisplay.rotation
+                // lewoPrawo = Przechylenie telefonu w lewo(90) i prawo(-90) + korekcja aparatu
+                val lewoPrawo = (event.values[0].toDouble() * 9) + 0.3
 
-                //sprawdzenie czy telefon znajduje się w trybie "landscape"
-                if (orientation == Surface.ROTATION_90 || orientation == Surface.ROTATION_270) {
+                // goraDol = Przechylenie telefonu: przedni aparat wyżej(90), płasko - ekranem w stronę sufitu (0),
+                // pasek nawigacyjny wyżej (-90) + korekcja aparatu
+                val goraDol = (event.values[1].toDouble() * 9) + 3.7
 
-                    // lewoPrawo = Przechylenie telefonu w lewo(-90) i prawo(90)
-                    val lewoPrawo = (event.values[1].toDouble() * 9) + 3.7
-
-                    // goraDol = Przechylenie telefonu: przyciski do kontroli głośności wyżej(90),
-                    // płasko - ekranem w stronę sufitu (0), przyciski do kontroli głośności niżej (-90) + korekcja aparatu
-                    val goraDol = (event.values[0].toDouble() * 9) + 0.3
-
-                    // Przesuwanie koła na podstawie przechylenia telefonu
-                    kolo.apply {
-                        translationX = (lewoPrawo * 4).toFloat()
-                        translationY = (goraDol * 4).toFloat()
-                    }
-
-
-                    kwadrat.text = "góra/dół ${"%.1f".format(goraDol)}°,\n lewo/prawo ${
-                        "%.1f".format(lewoPrawo)
-                    }°"
-                    if (goraDol >= -0.8 && goraDol <= 0.8 && lewoPrawo >= -0.8 && lewoPrawo <= 0.8) {
-                        info.text = "Płaszczyzna mieści się w normach"
-                        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-                        val pattern = longArrayOf(50, 150, 50)
-                        // Wywołaj wibrację
-                        vibrator.vibrate(pattern, -1)
-                    } else {
-                        info.text = "Płaszczyzna nie jest w normie"
-                    }
-                    if (goraDol == 0.0 && lewoPrawo == 0.0) {
-                        info.text = "Idealnie w poziomie"
-                    }
-
+                // Przesuwanie koła na podstawie przechylenia telefonu
+                kolo.apply {
+                    translationX = (lewoPrawo * -4).toFloat()
+                    translationY = (goraDol * 4).toFloat()
                 }
-                else {
-                    //sprawdzenie czy telefon znajduje się w trybie "portrait"
-                    // lewoPrawo = Przechylenie telefonu w lewo(90) i prawo(-90) + korekcja aparatu
-                    val lewoPrawo = (event.values[0].toDouble() * 9) + 0.3
-
-                    // goraDol = Przechylenie telefonu: przedni aparat wyżej(90), płasko - ekranem w stronę sufitu (0),
-                    // pasek nawigacyjny wyżej (-90) + korekcja aparatu
-                    val goraDol = (event.values[1].toDouble() * 9) + 3.7
-
-                    // Przesuwanie koła na podstawie przechylenia telefonu
-                    kolo.apply {
-                        translationX = (lewoPrawo * -4).toFloat()
-                        translationY = (goraDol * 4).toFloat()
-                    }
 
 
-                    kwadrat.text = "góra/dół ${"%.1f".format(goraDol)}°,\n lewo/prawo ${
-                        "%.1f".format(-lewoPrawo)
-                    }°"
-                    if (goraDol >= -0.8 && goraDol <= 0.8 && lewoPrawo >= -0.8 && lewoPrawo <= 0.8) {
-                        info.text = "Płaszczyzna mieści się w normach"
-                        val vibrator: Vibrator =
-                            getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-                        val pattern = longArrayOf(50, 150, 50)
-                        // Wywołaj wibrację
-                        vibrator.vibrate(pattern, -1)
-                    } else {
-                        info.text = "Płaszczyzna nie jest w normie"
-                    }
-                    if (goraDol == 0.0 && lewoPrawo == 0.0) {
-                        info.text = "Idealnie w poziomie"
-                    }
-
+                kwadrat.text = "góra/dół ${"%.1f".format(goraDol)}°,\n lewo/prawo ${
+                    "%.1f".format(-lewoPrawo)
+                }°"
+                if (goraDol >= -0.8 && goraDol <= 0.8 && lewoPrawo >= -0.8 && lewoPrawo <= 0.8) {
+                    info.text = "Płaszczyzna mieści się w normach"
+                    val vibrator: Vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+                    val pattern = longArrayOf(50, 150, 50)
+                    // Wywołaj wibrację
+                    vibrator.vibrate(pattern, -1)
+                    info.background.setColorFilter(ContextCompat.getColor(this, R.color.green), PorterDuff.Mode.SRC_ATOP)
+                } else {
+                    info.text = "Płaszczyzna nie jest w normie"
+                    info.background.setColorFilter(ContextCompat.getColor(this, R.color.red), PorterDuff.Mode.SRC_ATOP)
                 }
+
+                if (goraDol == 0.0 && lewoPrawo == 0.0) {
+                    info.text = "Idealnie w poziomie"
+                }
+
             }
+
         }
     }
 
